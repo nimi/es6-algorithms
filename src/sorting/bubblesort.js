19685f9c5df33fb55ed559031a8ff497dbe7b7df
Nicholas Mitchell <mitchellnicholasl@gmail.com>
2016-01-24 17:04:11 -0500

    Add es6 bubblesort

--- src/sorting/bubblesort.js
+++ src/sorting/bubblesort.js
@@ -1,42 +1,43 @@
-(function (exports) {
-  'use strict';
+import 'babel-polyfill';
 
-  function comparator(a, b) {
-    return a - b;
-  }
+function* range(begin, end, interval = 1) {
+	for (let i = begin; i < end; i += interval) { yield i; }
+}
 
-  /**
-   * Bubble sort algorithm.<br><br>
-   * Complexity: O(N^2).
-   *
-   * @example
-   * var sort = require('path-to-algorithms/src/' +
-   * 'sorting/bubblesort').bubbleSort;
-   * console.log(sort([2, 5, 1, 0, 4])); // [ 0, 1, 2, 4, 5 ]
-   *
-   * @public
-   * @module sorting/bubblesort
-   * @param {Array} array Input array.
-   * @param {Function} cmp Optional. A function that defines an
-   * alternative sort order. The function should return a negative,
-   * zero, or positive value, depending on the arguments.
-   * @return {Array} Sorted array.
-   */
-  function bubbleSort(array, cmp) {
-    cmp = cmp || comparator;
-    var temp;
-    for (var i = 0; i < array.length; i += 1) {
-      for (var j = i; j > 0; j -= 1) {
-        if (cmp(array[j], array[j - 1]) < 0) {
-          temp = array[j];
-          array[j] = array[j - 1];
-          array[j - 1] = temp;
-        }
-      }
-    }
-    return array;
-  }
+/**
+ * Bubble sort algorithm.<br><br>
+ * Complexity: O(N^2).
+ *
+ * @example
+ * var sort = require('path-to-algorithms/src/' +
+ * 'sorting/bubblesort').bubbleSort;
+ * console.log(sort([2, 5, 1, 0, 4])); // [ 0, 1, 2, 4, 5 ]
+ *
+ * @public
+ * @module sorting/bubblesort
+ * @param {Array} array Input array.
+ * @param {Function} cmp Optional. A function that defines an
+ * alternative sort order. The function should return a negative,
+ * zero, or positive value, depending on the arguments.
+ * @return {Array} Sorted array.
+ */
 
-  exports.bubbleSort = bubbleSort;
+export function bubbleSort(array, cmp = (a, b) => a - b) {
+	const swap = index => {
+		const temp = array[index];
+		array[index] = array[index - 1];
+		array[index - 1] = temp;
+	};
 
-})(typeof window === 'undefined' ? module.exports : window);
+	//	array.forEach((_, outerIndex) => {
+	for (let outerIndex of range(0, array.length)) {
+//		for (let innerIndex = outerIndex; innerIndex > 0; innerIndex -= 1) {
+		for (let innerIndex of range(outerIndex, 0, -1))
+		if (cmp(array[innerIndex], array[innerIndex - 1]) < 0) {
+				swap(innerIndex);
+			}
+		}
+	}
+//	});
+	return array;
+}
