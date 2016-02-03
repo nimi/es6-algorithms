f94660c0446bc0a10dc012a3cbce7ff416b608be
Nicholas Mitchell <mitchellnicholasl@gmail.com>
2016-02-03 18:37:02 -0500

    Update tests for oddeven

--- test/sorting/oddeven-sort.spec.js
+++ test/sorting/oddeven-sort.spec.js
@@ -1,18 +1,18 @@
 var oes =
-  require('../../src/sorting/oddeven-sort').oddEvenSort;
+  require('../../lib/sorting/oddeven-sort').oddEvenSort;
 
 describe('oddeven-sort', function () {
   'use strict';
 
   it('should sort the empty array', function () {
-    expect(oes([])).toEqual([]);
+	expect(oes([])).toEqual([]);
   });
 
   it('should return array with the same count of elements', function () {
-    expect(oes([2, 3, 4]).length).toBe(3);
+	expect(oes([2, 3, 4]).length).toBe(3);
   });
 
   it('should sort the given array in ascending order', function () {
-    expect(oes([42, 3, 10])).toEqual([3, 10, 42]);
+	expect(oes([42, 3, 10])).toEqual([3, 10, 42]);
   });
 });
