2e82564a29110c216abaa3c4b30a163c0703f67c
Nicholas Mitchell <mitchellnicholasl@gmail.com>
2016-02-10 18:28:12 -0500

    Fix failing tests for heap sort

--- test/sorting/heapsort.spec.js
+++ test/sorting/heapsort.spec.js
@@ -1,4 +1,4 @@
 var sortTestCase = require('./sort.testcase.js');
-var heapSort = require('../../src/sorting/heapsort.js').heapSort;
+var heapSort = require('../../lib/sorting/heapsort.js').heapSort;
 
 sortTestCase(heapSort, 'Heap sort');
