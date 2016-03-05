9dbdf60170c958fdb615a398ab1a8db1aea20996
Nicholas Mitchell <mitchellnicholasl@gmail.com>
2016-03-05 11:06:49 -0500

    Add insertion sort

--- test/sorting/insertionbinarysort.spec.js
+++ test/sorting/insertionbinarysort.spec.js
@@ -1,6 +1,6 @@
 var sortTestCase = require('./sort.testcase.js');
 var insertionBinarySort =
-      require('../../src/sorting/' +
-      'insertion-binary-sort.js').insertionBinarySort;
+	  require('../../lib/sorting/' +
+	  'insertion-binary-sort.js').insertionBinarySort;
 
 sortTestCase(insertionBinarySort, 'Insertion binary sort');
